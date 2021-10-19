0663b3453344e1bb37155c43d38353fae41be1cf
JoseBorbon <joserborbon@gmail.com>
2021-10-19 09:32:13 -0400

    added colors data

---
+++ src/data/colors.js
@@ -0,0 +1,17 @@
+const colors = [
+  'red',
+  'green',
+  'blue',
+  'yellow',
+  'orange',
+  'pink',
+  'white',
+  'black',
+  'purple',
+  'magenta',
+  'cyan',
+  'indigo',
+  'teal',
+];
+
+export default colors;
